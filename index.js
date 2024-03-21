36c42f8ce5982bc4bb7614139aecdc2d833ca2ce
prince mumo <princeinkunzi@gmail.com>
2024-03-21 05:43:19 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(symbol = "*") {
+    return function(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
+
+module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective
+};
+
